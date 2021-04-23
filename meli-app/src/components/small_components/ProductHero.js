e96725a8e8e7be50bb18318f9c52df6b566e2754
Joaquin <jtornello2019@hotmail.com>
2021-04-23 18:21:43 -0300

    Added ProductHero and styles

---
+++ meli-app/src/components/small_components/ProductHero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ProductSummary from './ProductSummary';
+
+const ProductHero = () => {
+  return (
+    <div className='hero'>
+      <img
+        className='hero__image--small product-result__image'
+        src='https://http2.mlstatic.com/D_NQ_NP_908744-MLA44099561456_112020-V.webp'
+        srcSet=''
+        alt='Imagen del producto'
+      />
+      <div className='hero__content'>
+        <ProductSummary reversed={true} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductHero;
