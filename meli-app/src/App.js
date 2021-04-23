735eac9c999add9ca1a9a4ac32f2a4574445a104
Joaquin <jtornello2019@hotmail.com>
2021-04-23 18:35:00 -0300

    Improved App.js

--- meli-app/src/App.js
+++ meli-app/src/App.js
@@ -1,8 +1,19 @@
-import SearchBox from './components/blocks/SearchBox';
+import React from 'react';
 import Navbar from './components/blocks/Navbar';
+import SearchResults from './components/pages/SearchResults';
+import ProductSummary from './components/small_components/ProductSummary';
+import BreadCrumb from './components/blocks/BreadCrumb';
+import ProductResult from './components/small_components/ProductResult';
+import ProductList from './components/blocks/ProductList';
+import ProductDetail from './components/blocks/ProductDetail';
+import ProductHero from './components/small_components/ProductHero';
 
-function App() {
-  return <Navbar />;
-}
+const App = () => {
+  return (
+    <React.Fragment>
+      <SearchResults />
+    </React.Fragment>
+  );
+};
 
 export default App;
