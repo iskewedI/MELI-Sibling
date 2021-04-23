b966e19dac8e12a4ba908a75ee63ff41f52c3dda
Joaquin <jtornello2019@hotmail.com>
2021-04-23 18:34:06 -0300

    Improved Product Summary

--- meli-app/src/components/small_components/ProductSummary.js
+++ meli-app/src/components/small_components/ProductSummary.js
@@ -1,31 +1,18 @@
 import React from 'react';
 
-const ProductSummary = ({ shipping = false }) => {
+const ProductSummary = ({ reversed }) => {
   return (
-    <div>
-      <article
-        className={`grid grid--cols-3 product-summary ${
-          shipping ? 'product-summary--shipping' : ''
-        }`}
-      >
-        <img
-          className='product-summary__image'
-          src='https://http2.mlstatic.com/D_NQ_NP_908744-MLA44099561456_112020-V.webp'
-          srcSet=''
-          alt='Imagen del producto'
-        />
-        <div className='product-summary__content'>
-          <h3 className='product-summary__heading'>
-            <span className='product-summary__money-symbol'>$</span>
-            1.545
-          </h3>
-          <p className='product-summary__description'>
-            Apple Ipod Touch 5g 16gb Negro Igual A Nuevo Completo Unico!
-          </p>
-        </div>
-        <div className='product-summary_location'>Capital Federal</div>
-      </article>
-    </div>
+    <article className={`product-summary ${reversed ? 'product-summary--reversed' : ''}`}>
+      <div className='product-summary__content'>
+        <h3 className='product-summary__heading'>
+          <span className='product-summary__money-symbol'>$</span>
+          1.545
+        </h3>
+        <p className='product-summary__description'>
+          Apple Ipod Touch 5g 16gb Negro Igual A Nuevo Completo Unico!{' '}
+        </p>
+      </div>
+    </article>
   );
 };
 
