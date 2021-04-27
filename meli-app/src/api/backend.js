8e0f0790122676b090e883c33660ddb771ce7516
Joaquin <jtornello2019@hotmail.com>
2021-04-27 12:05:16 -0300

    Provided caller name and caller lastname to backend api

--- meli-app/src/api/backend.js
+++ meli-app/src/api/backend.js
@@ -3,10 +3,14 @@ import { external } from '../config.json';
 
 const { backendAPI } = external;
 
+const getHeaders = () => {
+  return { caller_name: 'joaquin', caller_lastname: 'tornello' };
+};
+
 export const getSearchResults = async query => {
   const url = `${backendAPI.baseURL}/items?q=${query}`;
 
-  const result = await axios.get(url);
+  const result = await axios.get(url, { headers: getHeaders() });
 
   return result.data;
 };
@@ -14,7 +18,7 @@ export const getSearchResults = async query => {
 export const getProductDetail = async id => {
   const url = `${backendAPI.baseURL}/items/${id}`;
 
-  const result = await axios.get(url);
+  const result = await axios.get(url, { headers: getHeaders() });
 
   return result.data;
 };
