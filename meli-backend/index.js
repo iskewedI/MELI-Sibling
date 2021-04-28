514c3b966a66cdadfa1dee25849c604480900535
Joaquin <jtornello2019@hotmail.com>
2021-04-28 10:14:18 -0300

    Improved logging system of backend

--- meli-backend/index.js
+++ meli-backend/index.js
@@ -1,11 +1,10 @@
+const winston = require('winston');
 const app = require('express')();
 
 require('./startup/logging')(); //Api Validation initialization
 require('./startup/routes')(app); //Routes settings
 
 const port = process.env.PORT || 443;
-const server = app.listen(port, () =>
-  console.log(`Initializing on port ${port}...`)
-);
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
--- meli-backend/startup/logging.js
+++ meli-backend/startup/logging.js
@@ -1,8 +1,26 @@
+const winston = require('winston'); //Logger
 require('express-async-errors'); //Envolves router async functions in try/catch blocks
 
 module.exports = function () {
+  //Uncaught exception configs
+  winston.exceptions.handle(
+    new winston.transports.File({ filename: '../uncaughtExceptions.log' }),
+    new winston.transports.Console({ format: winston.format.simple() })
+  );
+
   //Trick to handle Promise Rejections too with winston
-  process.on('unhandledRejection', (ex) => {
-    console.error('An unhandled error has ocurred => ', ex);
+  process.on('unhandledRejection', ex => {
+    // Handled as Uncaught Exception
+    throw ex;
   });
+
+  //Logger configuration
+  winston.add(new winston.transports.File({ filename: '../logfile.log' }));
+
+  winston.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+      level: 'info',
+    })
+  );
 };
